e99ece6cf902197b54d681ab4a6ab9e60823c3ca
uidev-in <mail2loveseries@gmail.com>
2021-12-19 16:10:00 +0530

    added new content in products details part

---
+++ neofeed-product-details.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<?php include 'include/head.php';?>
+<style>
+.product-details-img {
+    margin-right: 160px;
+}
+.shop-thumb-tab {
+    width: 146px;
+}
+
+</style>
+    <body>
+        <!-- header -->
+            <?php include 'include/header.php';?>
+        <!-- header-end -->
+
+        <!-- Main Content -->
+        <main>
+            <!-- breadcrumb-area -->
+            <section class="breadcrumb-area d-flex align-items-center" style="background-image:url(img/testimonial/test-bg.png)">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-12 col-lg-12">
+                            <div class="breadcrumb-wrap text-left">
+                                <div class="breadcrumb-title mb-30">
+                                    <h2>Product Details</h2>    
+                                    <div class="breadcrumb-wrap">
+                              
+                                <nav aria-label="breadcrumb">
+                                    <ol class="breadcrumb">
+                                        <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                                        <li class="breadcrumb-item active" aria-current="page">Pricing</li>
+                                    </ol>
+                                </nav>
+                            </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </section>
+            <!-- breadcrumb-area-end -->
+
+            <!-- shop-banner-area start -->
+            <section class="shop-banner-area pt-100 pb-70 " data-animation="fadeInUp animated" data-delay=".2s">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-7">
+                        <!-- <div class="shop-thumb-tab mb-30">
+                                <ul class="nav" id="myTab2" role="tablist">
+                                    <li class="nav-item">
+                                        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab"
+                                            aria-selected="true"><img src="img/shop/details/thumb1.jpg" alt=""> </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab"
+                                            aria-selected="false"><img src="img/shop/details/thumb2.jpg" alt=""></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" id="profile-tab2" data-toggle="tab" href="#profile1" role="tab"
+                                            aria-selected="false"><img src="img/shop/details/thumb3.jpg" alt=""></a>
+                                    </li>
+                                </ul>
+                            </div> -->
+                            <div class="product-details-img mb-30 mr-5">
+                                <div class="tab-content" id="myTabContent2">
+                                    <div class="tab-pane fade show active" id="home" role="tabpanel">
+                                        <div class="product-large-img">
+                                            <img src="img/shop/img6.jpg" alt="">
+                                        </div>
+                                    </div>
+                                   <!--  <div class="tab-pane fade" id="profile" role="tabpanel">
+                                        <div class="product-large-img">
+                                            <img src="img/shop/details/large2.jpg" alt="">
+                                        </div>
+                                    </div>
+                                    <div class="tab-pane fade" id="profile1" role="tabpanel">
+                                        <div class="product-large-img">
+                                            <img src="img/shop/details/large3.jpg" alt="">
+                                        </div>
+                                    </div> -->
+                                </div>
+                            </div>
+
+                           
+
+                        </div>
+                        <div class="col-xl-5">
+                            <div class="product-details mb-30">
+                                <div class="product-details-title">
+                                    <p>Category</p>
+                                    <h1>Neofeed Powder</h1>
+                                    <div class="price details-price pb-30 mb-20">
+                                        <span class="text-dark font-weight-bold">Price : </span>
+                                        <span class="text-success font-weight-bold"> ₹1199.00</span>
+                                        <span class="text-dark"> / 400gm</span>
+                                    </div>
+                                </div>
+
+                                <h6 class="">Indications : </h6>
+                                <ul style="line-height:28px;">
+                                    <li><i class="fas fa-check text-success mr-2"></i>Malabsorptive Conditions</li>
+                                    <li><i class="fas fa-check text-success mr-2"></i>Lactose Intolerance</li>
+                                    <li><i class="fas fa-check text-success mr-2"></i>Multiple Food Allergy</li>
+                                    <li><i class="fas fa-check text-success mr-2"></i>Celiac Disease</li>
+                                    <li><i class="fas fa-check text-success mr-2"></i>Short Bowel Syndrome</li>
+                                    <li><i class="fas fa-check text-success mr-2"></i>Diarrhoea of any etiolgy</li>
+                                    <li><i class="fas fa-check text-success mr-2"></i>Weight gain in LBW and Premature Babies</li>
+                                    <li><i class="fas fa-check text-success mr-2"></i>GI tract surgery</li>
+                                </ul>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- shop-banner-area end -->
+
+            <!-- product-desc-area start -->
+            <section class="product-desc-area pb-55">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="bakix-details-tab">
+                                <ul class="nav text-left pb-30 mb-50" id="myTab" role="tablist">
+                                    <li class="nav-item">
+                                        <a class="nav-link active" id="desc-tab" data-toggle="tab" href="#id-desc" role="tab"
+                                            aria-controls="home" aria-selected="true">Benefits </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" id="id-add-in" data-toggle="tab" href="#id-add" role="tab"
+                                            aria-controls="profile" aria-selected="false">Additional Information</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="tab-content" id="myTabContent">
+                                <div class="tab-pane fade show active" id="id-desc" role="tabpanel" aria-labelledby="desc-tab">
+                                    <div class="event-text mb-40">
+                                    <ul style="line-height:28px;">
+                                        <li>» Contains Predigested chicken powder hence gentle on neo GI System Chicken protein supports faster weight gain</li>
+                                        <li>» Hypo allergenic : Lactose free, gluten free and surcose free. Does not cause complications</li>
+                                        <li>» Hypo osmolar : osmolality 70 mosm/kg H2O thus increase butrient absorption in blood</li>
+                                        <li>» Vitamins and Minerals fullfill increased nutrients requirement</li>
+                                        <li>» Choline helps proper Brain Developments</li>
+                                        <li>» Energy rich support normal growth : Maltodextrin, Pre cooked starch and coconut Powder (MCT)</li>
+                                        <li>» Neofeed is free from gulten and lactose</li>
+                                        
+                                    </ul>
+                                 </div>
+                                </div>
+                                <div class="tab-pane fade" id="id-add" role="tabpanel" aria-labelledby="id-add-in">
+                                    <div class="additional-info">
+                                        <div class="table-responsive">
+                                            <h4>Additional information</h4>
+                                            <table class="table">
+                                                <tbody>
+                                                    <tr>
+                                                        <th>Weight</th>
+                                                        <td class="product_weight">1.4 oz</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Dimensions</th>
+                                                        <td class="product_dimensions">62 × 56 × 12 in</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Size</th>
+                                                        <td class="product_dimensions">XL, XXL, LG, SM, MD</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- product-desc-area end -->
+
+        </main>
+        <!-- Main Content end-->
+
+        <!-- footer -->
+        <?php include 'include/footer.php';?>
+        <!-- footer-end -->
+
+		<!-- JS here -->
+        <?php include 'include/script.php';?>
+    </body>
+
+</html>
+
---
+++ puralbumen-product-details.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<?php include 'include/head.php';?>
+<style>
+.product-details-img {
+    margin-right: 160px;
+}
+.shop-thumb-tab {
+    width: 146px;
+}
+
+</style>
+    <body>
+        <!-- header -->
+            <?php include 'include/header.php';?>
+        <!-- header-end -->
+
+        <!-- Main Content -->
+        <main>
+            <!-- breadcrumb-area -->
+            <section class="breadcrumb-area d-flex align-items-center" style="background-image:url(img/testimonial/test-bg.png)">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-12 col-lg-12">
+                            <div class="breadcrumb-wrap text-left">
+                                <div class="breadcrumb-title mb-30">
+                                    <h2>Product Details</h2>    
+                                    <div class="breadcrumb-wrap">
+                              
+                                <nav aria-label="breadcrumb">
+                                    <ol class="breadcrumb">
+                                        <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                                        <li class="breadcrumb-item active" aria-current="page">Pricing</li>
+                                    </ol>
+                                </nav>
+                            </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </section>
+            <!-- breadcrumb-area-end -->
+
+            <!-- shop-banner-area start -->
+            <section class="shop-banner-area pt-100 pb-70 " data-animation="fadeInUp animated" data-delay=".2s">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-7">
+                        <!-- <div class="shop-thumb-tab mb-30">
+                                <ul class="nav" id="myTab2" role="tablist">
+                                    <li class="nav-item">
+                                        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab"
+                                            aria-selected="true"><img src="img/shop/details/thumb1.jpg" alt=""> </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab"
+                                            aria-selected="false"><img src="img/shop/details/thumb2.jpg" alt=""></a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" id="profile-tab2" data-toggle="tab" href="#profile1" role="tab"
+                                            aria-selected="false"><img src="img/shop/details/thumb3.jpg" alt=""></a>
+                                    </li>
+                                </ul>
+                            </div> -->
+                            <div class="product-details-img mb-30 mr-5">
+                                <div class="tab-content" id="myTabContent2">
+                                    <div class="tab-pane fade show active" id="home" role="tabpanel">
+                                        <div class="product-large-img">
+                                            <img src="img/shop/img5.jpg" alt="">
+                                        </div>
+                                    </div>
+                                    <!-- <div class="tab-pane fade" id="profile" role="tabpanel">
+                                        <div class="product-large-img">
+                                            <img src="img/shop/details/large2.jpg" alt="">
+                                        </div>
+                                    </div>
+                                    <div class="tab-pane fade" id="profile1" role="tabpanel">
+                                        <div class="product-large-img">
+                                            <img src="img/shop/details/large3.jpg" alt="">
+                                        </div>
+                                    </div> -->
+                                </div>
+                            </div>
+
+                           
+
+                        </div>
+                        <div class="col-xl-5">
+                            <div class="product-details mb-30">
+                                <div class="product-details-title">
+                                    <p>Category</p>
+                                    <h1>Puralbumen Powder</h1>
+                                    <div class="price details-price pb-30 mb-20">
+                                        <span class="text-dark font-weight-bold">Price : </span>
+                                        <span class="text-success font-weight-bold"> ₹1399.00</span>
+                                        <span class="text-dark"> / 400gm</span>
+                                    </div>
+                                </div>
+                                <h6 class="">Indications : </h6>
+                                <ul style="line-height:28px;">
+                                    <li><i class="fas fa-check text-success mr-2"></i>Hypoalbuminemia</li>
+                                    <li><i class="fas fa-check text-success mr-2"></i>Liver Cirrhosis</li>
+                                    <li><i class="fas fa-check text-success mr-2"></i>Tissue repair conditons</li>
+                                    <li><i class="fas fa-check text-success mr-2"></i>Cancer</li>
+                                    <li><i class="fas fa-check text-success mr-2"></i>Cachexia</li>
+                                    <li><i class="fas fa-check text-success mr-2"></i>COPD</li>
+                                    <li><i class="fas fa-check text-success mr-2"></i>TB</li>
+                                    
+                                </ul>
+                                <!-- <h6 class=" mt-3">Benefits : </h6> -->
+                               
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- shop-banner-area end -->
+
+            <!-- product-desc-area start -->
+            <section class="product-desc-area pb-55">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="bakix-details-tab">
+                                <ul class="nav text-left pb-30 mb-50" id="myTab" role="tablist">
+                                    <li class="nav-item">
+                                        <a class="nav-link active" id="desc-tab" data-toggle="tab" href="#id-desc" role="tab"
+                                            aria-controls="home" aria-selected="true">Benefits </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" id="id-add-in" data-toggle="tab" href="#id-add" role="tab"
+                                            aria-controls="profile" aria-selected="false">Additional Information</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="tab-content" id="myTabContent">
+                                <div class="tab-pane fade show active" id="id-desc" role="tabpanel" aria-labelledby="desc-tab">
+                                    <div class="event-text mb-40">
+                                    <ul style="line-height:28px;">
+                                    <li>» 65% High Biological value albumen Protein to increase serum albumin level</li>
+                                    <li>» Glutamine and Arginine helps boot immunity and prevents secondary infection</li>
+                                    <li>» DHA &amp; helps in reducing  inflammation and brain development</li>
+                                    <li>» Mixed Carotenoids and Lycopene Powerful antioxidant scanvenge free radicals</li>
+                                    <li>» Vitamins and Minerals fullfill increased nutritional need and reduces fatigue</li>
+                                    <li>» Fullfills in protein requirements from 1.2g to 1.5g/kg/BW/day</li>
+                                    <li>» BCAA helps in maintaining positive nitrogen balance</li>
+                                    <li>» Puralbumen is free from Lactose, Gluten,Cholesterol and Sugar. </li>
+                                    
+                                </ul>
+                                    </div>
+                                </div>
+                                <div class="tab-pane fade" id="id-add" role="tabpanel" aria-labelledby="id-add-in">
+                                    <div class="additional-info">
+                                        <div class="table-responsive">
+                                            <h4>Additional information</h4>
+                                            <table class="table">
+                                                <tbody>
+                                                    <tr>
+                                                        <th>Weight</th>
+                                                        <td class="product_weight">1.4 oz</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Dimensions</th>
+                                                        <td class="product_dimensions">62 × 56 × 12 in</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Size</th>
+                                                        <td class="product_dimensions">XL, XXL, LG, SM, MD</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- product-desc-area end -->
+
+        </main>
+        <!-- Main Content end-->
+
+        <!-- footer -->
+        <?php include 'include/footer.php';?>
+        <!-- footer-end -->
+
+		<!-- JS here -->
+        <?php include 'include/script.php';?>
+    </body>
+
+</html>
+
